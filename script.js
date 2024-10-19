09fb747a279b575be51553db7d98e7a63fddbf00
PauloVisbeck <pls_ray@yahoo.com.br>
2024-10-18 21:25:01 -0300

    Segunda versao. Modificacao nos arquivos index.html e script.js para otimizar algumas funções através da exclusao de codigo redundante. Criacao de funcao especifica para formatacao de moeda.

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-let cardapio = document.getElementById("cardapio")
+const cardapio = document.getElementById("cardapio")
+const mostrarCardapio = document.querySelector(".listarCardapio")
+const aplicarDdesconto = document.querySelector(".aplicarDdesconto")
 
-function mostrar_tudo() {    
+
+function formatarTipoMoeda(valor) {
+    const valorFormatadoMoeda = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(valor)
+
+    return valorFormatadoMoeda
+}
+
+
+function mostrar(arrayProdutos) {
     cardapio.innerHTML = ""
-   
-    
-    menuOptions.forEach(element => {
+
+    arrayProdutos.forEach(element => {
         let li = document.createElement("li")
 
-        const valorFormatadoMoeda = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        }).format(element.preco)
-
-        
         li.innerHTML = `            
             <img src="${element.src}" alt="${element.nome}">
             <p>${element.nome}</p>
-            <p class="preco-item">${valorFormatadoMoeda}</p>            
-        `        
-        
+            <p class="preco-item">${formatarTipoMoeda(element.preco)}</p>            
+        `
+
         cardapio.appendChild(li)
     });
 }
 
 
-function aplicar_desconto() {    
+function aplicaDescontoCardapio() {
     cardapio.innerHTML = ""
-    
+
     const novoMenuOptions = menuOptions.map((item) => {
         const novoItem = {
             nome: item.nome,
@@ -36,66 +42,36 @@ function aplicar_desconto() {
         }
         return novoItem
     })
-    
-    novoMenuOptions.forEach(element => {        
-        let li = document.createElement("li")
-       
-        const valorFormatadoMoeda = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        }).format(element.preco)
-        
-        li.innerHTML = `            
-            <img src="${element.src}" alt="${element.nome}">
-            <p>${element.nome}</p>
-            <p class="preco-item">${valorFormatadoMoeda}</p>            
-        `        
-        cardapio.appendChild(li)
-    });
+
+    mostrar(novoMenuOptions)
 }
 
 
-function valor_total() {  
+function valor_total() {
     cardapio.innerHTML = ""
-    
+
     const valorTotalCardapio = menuOptions.reduce((acumulador, Item) => {
         return acumulador + Item.preco
     }, 0)
-    
-    const valorFormatadoMoeda = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL"
-    }).format(valorTotalCardapio)
 
- 
     const li = document.createElement("li")
-    
+
     li.innerHTML = `
-        <p>A soma de todos os itens do menu é: <br> ${valorFormatadoMoeda}</p>
+        <p>A soma de todos os itens do menu é: <br> ${formatarTipoMoeda(valorTotalCardapio)}</p>
     `
-    
+
     cardapio.appendChild(li)
 }
 
-function exibe_veganos() {    
+
+function exibe_veganos() {
     cardapio.innerHTML = ""
-  
+
     const apenasVeganos = menuOptions.filter((hamburguer) => hamburguer.vegano ? true : false)
-    
-    apenasVeganos.forEach(hamburguer => {        
-        const li = document.createElement("li")
-       
-        const valorFormatadoMoeda = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        }).format(hamburguer.preco)
-        
-        li.innerHTML = `
-            <img src=${hamburguer.src} alt=${hamburguer.nome}>
-            <p>${hamburguer.nome}</p>
-            <p class="preco-item">${valorFormatadoMoeda}</p>
-        `
-        
-        cardapio.appendChild(li)
-    });
+
+    mostrar(apenasVeganos)
 }
+
+
+mostrarCardapio.addEventListener("click", () => mostrar(menuOptions))
+aplicarDdesconto.addEventListener("click", aplicaDescontoCardapio)
